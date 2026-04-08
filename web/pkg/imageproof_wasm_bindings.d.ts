abe9d07e77044aa69a3926848592865450d1032b
Woblaa Developer <woblaa@users.noreply.github.com>
2026-04-08 11:59:57 +0200

    chore: commit WASM pkg artifacts for Vercel deployment
    - Removed pkg/ from .gitignore so Vercel can build without Rust toolchain
    - Vercel only needs to run 'vite build' (no wasm-pack required in CI)

---
+++ web/pkg/imageproof_wasm_bindings.d.ts
@@ -0,0 +1,47 @@
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * One-time initialization: install a panic hook that forwards Rust panics
+ * to `console.error` so they are visible in browser DevTools instead of
+ * being swallowed as opaque "unreachable" WASM traps.
+ */
+export function init(): void;
+
+export function verify_image(image_bytes: Uint8Array, fast_mode: boolean): any;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly verify_image: (a: number, b: number, c: number) => [number, number, number];
+    readonly init: () => void;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __externref_table_dealloc: (a: number) => void;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
